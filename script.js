a7ba5c8425859cb3d876aad91d48d2d986ce97a9
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-03-26 09:14:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function makeid(l) {
   // write your code here
+
+	const str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+	const charLen = str.length
+	 let count = 0
+	 let ans = '';
+	while(count < l ) {
+		ans += str.charAt(Math.random() * charLen)   
+	    count += 1 
+	}
+	return ans 
+	
 }
 
 // Do not change the code below.
